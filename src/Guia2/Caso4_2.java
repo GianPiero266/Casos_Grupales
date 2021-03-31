a649180e91f616b460e4cda39ba3d06918565d86
PIERO <PIERO@PC-MASTER-RACE->
2021-03-31 11:52:13 -0500

    Guia 2 casos grupales

--- src/Guia2/Caso4_2.java
+++ src/Guia2/Caso4_2.java
@@ -1,5 +1,35 @@
 package Guia2;
 
-public class Caso4_2 {
+import java.util.Scanner;
 
+public class Caso4_2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese número 1: ");
+		int n1 = sc.nextInt();
+		
+		System.out.print("Ingrese número 2: ");
+		int n2 = sc.nextInt();
+		
+		System.out.print("Ingrese número 3: ");
+		int n3 = sc.nextInt();
+		
+		String mensaje = "";
+		
+		if (n1 == n2 && n1 == n3 && n2 == n3)
+			mensaje = "Los tres números son iguales";
+		else if (n1 < n2 && n1 < n3)
+			mensaje = "El primer número " + n1 + " es menor";
+		else if (n2 < n1 && n2 < n3)
+			mensaje = "El segundo número " + n2 + " es menor";
+		else
+			mensaje = "El tercer número " + n3 + " es menor";
+		
+		System.out.println("\n--- R E S U L T A D O ---");
+		System.out.println(mensaje);
+	}
 }
+
+
+
